7fd288ad7c02d4292e4caabfe596cdcf4643038a
Akula <akula@Akulas.local>
2024-07-11 22:09:48 +0530

    Initial commit

---
+++ ollamahandler.py
@@ -0,0 +1,89 @@
+import requests
+import json
+import time
+
+# URL of the API endpoint
+url = 'http://127.0.0.1:11434/api/chat'
+
+headers = {
+    "User-Agent": "ollama/0.1.48 (arm64 darwin) Go/go1.22.0",
+    "Accept": "application/x-ndjson",
+    "Content-Type": "application/json",
+}
+
+character_personality = """
+You are von, a passionate software developer and you have vast knowledge of 
+all technologies java, aws, kafka, kubernetes, docker, sql, multithreading, rest apis.
+
+You are attending an interview for software developer role. 
+Act from now on always in your role as the confident, 
+suggestive, without ever hinting that 
+you are an AI by providing examples to the questions that have been asked.
+
+You are now von.
+"""
+
+# Create a session object to maintain context
+session = requests.Session()
+
+def loadModel() :
+    # Initial request payload to load the model
+    global session
+    initial_payload = {
+        "model": "llama3:8b",
+        "messages": None,
+        "format": "",
+        "options": None
+    }
+
+    # Sending the initial request
+    initial_response = session.post(url, headers=headers, data=json.dumps(initial_payload))
+    if initial_response.status_code == 200:
+        print("Model loaded successfully.")
+    else:
+        print("Error loading model:", initial_response.status_code, initial_response.text)
+        exit()
+
+# Function to send the request and process responses
+def process(text):
+    global url, headers, session
+    payload = {
+        "model": "llama3:8b",
+        "messages": [
+            {"role": "system", "content": character_personality},
+            {"role": "user", "content": f"Hey von could you explain , {text}"}
+            ],
+        "format": "",
+        "options": {}
+    }
+    return session.post(url, headers=headers, json=payload, stream=True)
+
+def testOllamaModel():
+    start_time = time.time()
+    loadModel()
+    load_time = time.time() - start_time
+    print(f"Time taken to load model: {load_time:.2f} seconds")
+    for i in range(3):
+        start_time = time.time()
+        response = process(f"text white {i}")
+        process_time = time.time() - start_time
+        output = ""
+        for line in response.iter_lines():
+            if line:
+                data = json.loads(line)
+                output += data['message']['content']
+        print(output)
+        print(f"Time taken for process {i+1} : {process_time:.2f} seconds")
+
+def ollamaStop() :
+    global url, headers, session
+    payload = {
+        "model": "llama3:8b",
+        "messages": [
+            {"role": "user", "content": "/bye"}
+            ],
+        "format": "",
+        "options": {}
+    }
+    session.post(url, headers=headers, json=payload)
+    session.close()
---
+++ startAISolver.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Step 1: Check if Ollama is installed, if not, install it
+if ! command -v ollama &> /dev/null; then
+    echo "Ollama not found. Installing Ollama..."
+    curl -fsSL https://ollama.com/install.sh | sh
+else
+    echo "Ollama is already installed."
+    ollama --version
+fi
+
+# Step 2: Pull the llama3:8b model
+echo "Pulling llama3:8b model..."
+ollama pull llama3:8b
+
+# Step 4: Install Python requirements
+echo "Installing Python requirements..."
+pip install -r requirements.txt
+if [ $? -eq 0 ]; then
+    echo "Python requirements installed successfully."
+else
+    echo "Failed to install Python requirements."
+    exit 1
+fi
+
+# Function to clear the screen based on the operating system
+clear_screen() {
+    case "$(uname)" in
+        Darwin)
+            clear  # macOS
+            ;;
+        Linux)
+            clear  # Linux
+            ;;
+        CYGWIN*|MINGW32*|MSYS*|MINGW*)
+            cls  # Windows
+            ;;
+        *)
+            echo "Unsupported OS"
+            ;;
+    esac
+}
+
+# Call the function
+clear_screen
+
+
+# Step 5: Run the Python script
+echo "Running main py..."
+python speech-rtSTT.py
+if [ $? -eq 0 ]; then
+    echo "Python script ran successfully."
+else
+    echo "Failed to run Python script."
+    exit 1
+fi
